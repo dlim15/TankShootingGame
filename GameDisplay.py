888d0ec44ce101a5c7f702c19183cd84ea98cb4f
DV <d_lim10@u.pacific.edu>
2017-03-24 04:34:22 -0700

    Temporarily added two arrow key buttons that will move the tanks left and right.

--- GameDisplay.py
+++ GameDisplay.py
@@ -24,10 +24,18 @@ class GameScreen(BaseMenuDisplay):
         self.top.attributes("-fullscreen",False)
         self.top.show_frame(MainMenu)
 
+
+
     def addCanvas(self):
-        self.canvas = Canvas(self.topFrame)
-        self.canvas.pack()
-        self.canvas.place(x=0, y=30,width=super().winfo_screenwidth(), height=super().winfo_screenheight())
+        self.canvas = Canvas(self.topFrame,width=super().winfo_screenwidth(), height=super().winfo_screenheight())
+        self.canvas.pack(expand=1,fill=BOTH)
+        self.canvas.place(x=0, y=30)
+        self.btnLeft = Button(self.canvas,text="<-", font=("Arial", 20, "bold"), command=self.moveLeft);
+        self.btnRight = Button(self.canvas,text="->", font=("Arial", 20, "bold"), command=self.moveRight);
+        self.btnLeft.pack()
+        self.btnRight.pack()
+        self.btnLeft.place(x=100,y=super().winfo_screenheight()*7/8, width=35,height=30)
+        self.btnRight.place(x=150,y=super().winfo_screenheight()*7/8, width=35,height=30)
 
     def addTank(self):
         imgTank = PhotoImage(file="tank.gif")
@@ -35,7 +43,12 @@ class GameScreen(BaseMenuDisplay):
         self.lblTank.image = imgTank
         self.lblTank.pack()
         self.lblTank.place(x=0, y=super().winfo_screenheight()*2/3, width=200, height=104)
+    def moveLeft(self):
+        self.lblTank.place(x=self.lblTank.winfo_x()-10,y=self.lblTank.winfo_y())
 
+    def moveRight(self):
+        self.lblTank.place(x=self.lblTank.winfo_x()+10,y=self.lblTank.winfo_y())
+        # self.canvas.move(self.lblTank,10,0)
     # @staticmethod
     # def leftKey(event):
     #     print("left clicked")
